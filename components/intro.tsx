8b6267b1f1047e198796f4530626252390d4666a
cigsaditya <cigs.aditya@gmail.com>
2023-09-14 20:14:46 +0700

    remove llinkedin and change to linktree

--- components/intro.tsx
+++ components/intro.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
-import { FaGithubSquare } from "react-icons/fa";
+import { FaGithubSquare, FaLink } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
 
@@ -100,10 +100,10 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com/in/adityh/"
+          href="https://linktr.ee/adityaa17_"
           target="_blank"
         >
-          <BsLinkedin />
+          <FaLink />
         </a>
 
         <a
