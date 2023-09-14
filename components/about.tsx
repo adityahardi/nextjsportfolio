cac68abd3e9ff211dea4bb424bc7e5f2534e1578
cigsaditya <cigs.aditya@gmail.com>
2023-09-14 14:35:16 +0700

    change some text

--- components/about.tsx
+++ components/about.tsx
@@ -19,30 +19,26 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
+        interest in technology, especially{" "}
+        <span className="font-medium">Computers</span>, began when I used to
+        enjoy playing games at cybercafes. It was during my internship at a company that I discovered{" "}
+        <span className="font-medium">my passion for programming</span>.{" "}
+        <span className="italic"> I had the opportunity to work there as an intern,</span> and now I am
+        employed as a web developer. I <span className="underline">am</span> constantly
+        learning and improving my skills in various programming languages, frameworks, and technologies.
+        {" "}
         <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
+          Laravel, PHP, TALL-Stack
         </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
+        . I enjoy the challenges that come with developing 
+        web applications and finding creative solutions to problems{" "}
+        <span className="font-medium">full-time position</span> as a full-stack
         developer.
       </p>
 
       <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className="italic">When I'm not in work</span>, I like to explore
+        new technologies, contribute to open-source projects, and participate in coding competitions.{" "}
       </p>
     </motion.section>
   );
